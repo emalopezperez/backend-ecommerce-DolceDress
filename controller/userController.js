c3bf84c3101e54e100c61194e63127db90d0c496
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2024-02-23 10:33:03 -0300

    Implemented password hashing

--- controller/userController.js
+++ controller/userController.js
@@ -1,18 +1,24 @@
 const User = require("../models/userModel");
 
 const createUser = async (req, res) => {
-    const email = req.body.email;
-
-    const findUser = await User.findOne({ email: email });
-
-    if (!findUser) {
-        const newUser = await User.create(req.body);
-        res.json(newUser);
-    } else {
-        res.json({
-          message:"Usuario ya registrado",
-          success:false
-        })
+    try {
+        const email = req.body.email;
+        const findUser = await User.findOne({ email: email });
+        if (!findUser) {
+            const newUser = await User.create(req.body);
+            res.status(201).json(newUser); 
+        } else {
+            res.status(400).json({ 
+                message: "Usuario ya esta registrado",
+                success: false
+            });
+        }
+    } catch (error) {
+        console.error("Error al crear usuario:", error);
+        res.status(500).json({ 
+            message: "Ocurrió un error al crear el usuario",
+            success: false
+        });
     }
 }
 
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api/user", authRouter);
 
