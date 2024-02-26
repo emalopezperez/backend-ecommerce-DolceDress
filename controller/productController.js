d10c1b3f25340da388508ccc43672031107458f3
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2024-02-26 10:17:31 -0300

    Added endpoint to obtain all products

--- controller/productController.js
+++ controller/productController.js
@@ -37,7 +37,22 @@ const getProduct = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Error al recuperar el producto",
+      error: error.message,
+    });
+  }
+};
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
       error: error.message,
     });
   }
@@ -46,4 +61,5 @@ const getProduct = async (req, res) => {
 module.exports = {
   createProduct,
   getProduct,
+  getAllProducts,
 };
--- routes/productRuote.js
+++ routes/productRuote.js
@@ -4,6 +4,7 @@ const product = require("../controller/productController");
 const router = express.Router();
 
 router.post("/create-product", product.createProduct);
-router.get("/get-product/:id", product.getProduct);
+router.get("/get-products", product.getAllProducts);
+router.get("/:id", product.getProduct);
 
 module.exports = router;
