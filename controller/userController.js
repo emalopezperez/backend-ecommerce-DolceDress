4d3715f9628cbaf3ef2e12a6a69375f89f6468fb
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2024-03-13 06:47:27 -0300

    Creating order functionality is done

--- controller/userController.js
+++ controller/userController.js
@@ -2,12 +2,14 @@ const User = require("../models/userModel");
 const Cart = require("../models/cartModel");
 const Product = require("../models/productModel");
 const Coupon = require("../models/cuponModel");
+const Order = require("../models/orderModel");
 const sendEmail = require("../controller/emailController");
 const { generateToken } = require("../config/jwtToken");
 const { generateRefreshToken } = require("../config/refreshToken");
 
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+const uniqid = require("uniqid");
 
 const createUser = async (req, res) => {
   try {
@@ -559,6 +561,111 @@ const applyCoupon = async (req, res) => {
   }
 };
 
+const createOrder = async (req, res) => {
+  const { COD, couponApplied } = req.body;
+  const { _id } = req.user;
+
+  try {
+    if (!COD) throw new Error("Create cash order failed");
+    const user = await User.findById(_id);
+    let userCart = await Cart.findOne({ orderby: user._id });
+    let finalAmout = 0;
+    if (couponApplied && userCart.totalAfterDiscount) {
+      finalAmout = userCart.totalAfterDiscount;
+    } else {
+      finalAmout = userCart.cartTotal;
+    }
+
+    let newOrder = await new Order({
+      products: userCart.products,
+      paymentIntent: {
+        id: uniqid(),
+        method: "COD",
+        amount: finalAmout,
+        status: "Cash on Delivery",
+        created: Date.now(),
+        currency: "usd",
+      },
+      orderby: user._id,
+      orderStatus: "Cash on Delivery",
+    }).save();
+
+    let update = userCart.products.map((item) => {
+      return {
+        updateOne: {
+          filter: { _id: item.product._id },
+          update: { $inc: { quantity: -item.count, sold: +item.count } },
+        },
+      };
+    });
+
+    const updated = await Product.bulkWrite(update, {});
+    res.json({ message: "success" });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getOrders = async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const userorders = await Order.findOne({ orderby: _id })
+      .populate("products.product")
+      .populate("orderby")
+      .exec();
+    res.json(userorders);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getAllOrders = async (req, res) => {
+  try {
+    const alluserorders = await Order.find()
+      .populate("products.product")
+      .populate("orderby")
+      .exec();
+    res.json(alluserorders);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getOrderByUserId = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const userorders = await Order.findOne({ orderby: id })
+      .populate("products.product")
+      .populate("orderby")
+      .exec();
+    res.json(userorders);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const updateOrderStatus = async (req, res) => {
+  const { status } = req.body;
+  const { id } = req.params;
+
+  try {
+    const updateOrderStatus = await Order.findByIdAndUpdate(
+      id,
+      {
+        orderStatus: status,
+        paymentIntent: {
+          status: status,
+        },
+      },
+      { new: true }
+    );
+    res.json(updateOrderStatus);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 module.exports = {
   createUser,
   loginUser,
@@ -578,4 +685,9 @@ module.exports = {
   getUserCart,
   emptyCart,
   applyCoupon,
+  createOrder,
+  getOrders,
+  getAllOrders,
+  getOrderByUserId,
+  updateOrderStatus,
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,6 +10,7 @@ router.post("/admin-login", user.loginAdmin);
 router.post("/forgot-password", authMiddleware, user.forgotPassword);
 router.post("/save-address", authMiddleware, user.saveAddress);
 router.post("/cart", authMiddleware, user.userCart);
+router.post("/cash-order", authMiddleware, user.createOrder);
 router.post("/apply-coupon", authMiddleware, user.applyCoupon);
 
 router.put("/password", authMiddleware, user.updatePassword);
@@ -20,10 +21,19 @@ router.get("/logout", user.logout);
 router.get("/refresh", user.handleRefreshToken);
 router.get("/get-wishlist", authMiddleware, user.getWishlist);
 router.get("/get-cart", authMiddleware, user.getUserCart);
+router.get("/get-orders", authMiddleware, user.getOrders);
+router.get("/get-all-orders", authMiddleware, isAdmin, user.getAllOrders);
+router.get("/get-order/:id", authMiddleware, isAdmin, user.getAllOrders);
 router.get("/:id", authMiddleware, isAdmin, user.getUser);
 
 router.delete("/empty-cart", authMiddleware, user.emptyCart);
 router.delete("/delete/:id", authMiddleware, user.deleteUser);
 router.put("/edit/:id", authMiddleware, user.updateUser);
+router.put(
+  "/order/update-order/:id",
+  authMiddleware,
+  isAdmin,
+  user.updateOrderStatus
+);
 
 module.exports = router;
